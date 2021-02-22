1628ae29c261c2e76c1e5d5daa6da95fcebff320
Garland <garland@cloudronics.com>
2021-02-22 23:05:17 +0200

    Updates

---
+++ precache-manifest.757818e9cc4823ccdd50ae5d3bf376eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2871e580c47cfffb6ee52da49a0a2f",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "244284b265d6275dbe70",
+    "url": "/Todo-List/static/js/2.310bd791.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todo-List/static/js/2.310bd791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f795af06a22de143c0",
+    "url": "/Todo-List/static/js/main.016ba805.chunk.js"
+  },
+  {
+    "revision": "e6a73e60887bb7c0c63a",
+    "url": "/Todo-List/static/js/runtime-main.a3320050.js"
+  }
+]);
